4b5f785fdfb2e838e0882e6d35e6abb098fb088d
lucas-mior <lucas.mior@tutamail.com>
2024-01-01 12:55:18 -0300

    use high level IO

--- main.c
+++ main.c
@@ -27,44 +27,17 @@ int main(int argc, char **argv) {
     }
     printf("realpath: %s\n", file.name);
 
-    if ((file.fd = open(file.name, O_RDONLY)) < 0) {
+    if ((file.file = fopen(file.name, "r")) == NULL) {
         error("Error opening %s for reading: %s\n", file.name, strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    {
-        struct stat file_stat;
-        if (fstat(file.fd, &file_stat) < 0) {
-            error("Error getting file information: %s\n", strerror(errno));
-            util_close(&file);
-            exit(EXIT_FAILURE);
-        }
-        file.length = (usize) file_stat.st_size;
-        if (file.length <= 0) {
-            error("file_length: %zu\n", file.length);
-            util_close(&file);
-            exit(EXIT_FAILURE);
-        }
+    char buffer[BUFSIZ];
+    while (fgets(buffer, sizeof (buffer), file.file)) {
+        printf("fgets: %s\n", buffer);
     }
 
-    file.map = mmap(NULL, file.length, 
-                    PROT_READ | PROT_WRITE, MAP_PRIVATE,
-                    file.fd, 0);
-    if (file.map == MAP_FAILED) {
-        error("Error mapping file file to memory: %s", strerror(errno));
-        util_close(&file);
-        exit(EXIT_FAILURE);
-    }
-
-    for (char *p = file.map; p < (char *)(file.map + file.length); p += 1) {
-    }
-
-    if (munmap(file.map, file.length) < 0) {
-        error("Error unmapping %p with %zu bytes: %s\n",
-              (void *) file.map, file.length, strerror(errno));
-    }
     util_close(&file);
-
     exit(EXIT_SUCCESS);
 }
 
