cf51dbc01895388398cc8ad511ced094b9e21fd2
lucas-mior <lucas.mior@tutamail.com>
2024-01-01 12:23:16 -0300

    add test.csv and get realpath

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <limits.h>
 #include <stdlib.h>
 #include <libgen.h>
+#include <errno.h>
+#include <string.h>
 
 #include "csv_plotter.h"
 
@@ -9,10 +12,17 @@ char *program;
 static void usage(FILE *) __attribute__((noreturn));
 
 int main(int argc, char **argv) {
+    char *filename;
     program = basename(argv[0]);
 
     if (argc != 2)
         usage(stderr);
+
+    if ((filename = realpath(argv[1], NULL)) == NULL) {
+        error("Error getting realpath of %s: %s\n", argv[1], strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    printf("realpath: %s\n", filename);
 
     exit(EXIT_SUCCESS);
 }
