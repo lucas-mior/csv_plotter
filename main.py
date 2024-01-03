a53bd89446b82a8527f292a22b8903b5a6afede3
lucas-mior <lucas.mior@tutamail.com>
2024-01-03 18:56:26 -0300

    error function

--- main.py
+++ main.py
@@ -4,16 +4,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+def error(message):
+    print(f"{program}: {message}", file=sys.stderr)
+    return
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print(f"usage: {sys.argv[0]} <file.csv>", file=sys.stderr);
         exit(1);
 
-    filename = sys.argv[1];
+    program = sys.argv[0]
+    filename = sys.argv[1]
+    df = None
     try:
         df = pd.read_csv(filename)
     except Exception:
-        print(f"Error reading {filename}", file=sys.stderr);
+        error(f"Error reading {filename}")
+        exit(1)
+    if df is None:
+        error(f"Error reading {filename}")
+        exit(1)
 
     x = df.iloc[:, 0]
     for column in df.columns:
