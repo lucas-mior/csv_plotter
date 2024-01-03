696a44e822ceecf087aa81804cd2281ea6163cf6
lucas-mior <lucas.mior@tutamail.com>
2024-01-03 19:23:30 -0300

    pass example.py to main.py

--- main.py
+++ main.py
@@ -1,54 +1,92 @@
 import sys
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_gtk3 import FigureCanvasGTK3
+import numpy as np
+
 import gi
-gi.require_version('Gtk', '3.0')
+gi.require_version('Gtk', '4.0')
 from gi.repository import Gtk
+
+from matplotlib.backends.backend_gtk4 import \
+    NavigationToolbar2GTK4 as NavigationToolbar
+from matplotlib.backends.backend_gtk4agg import \
+    FigureCanvasGTK4Agg as FigureCanvas
+from matplotlib.figure import Figure
 
 def error(message):
     print(f"{program}: {message}", file=sys.stderr)
     return
 
+
+def on_activate(app):
+    win = Gtk.ApplicationWindow(application=app)
+    win.set_default_size(400, 300)
+    win.set_title("Embedding in GTK4")
+
+    fig = Figure(figsize=(5, 4), dpi=100)
+    ax = fig.add_subplot(1, 1, 1)
+    t = np.arange(0.0, 3.0, 0.01)
+    s = np.sin(2*np.pi*t)
+    ax.plot(t, s)
+
+    vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+    win.set_child(vbox)
+
+    # Add canvas to vbox
+    canvas = FigureCanvas(fig)  # a Gtk.DrawingArea
+    canvas.set_hexpand(True)
+    canvas.set_vexpand(True)
+    vbox.append(canvas)
+
+    # Create toolbar
+    toolbar = NavigationToolbar(canvas)
+    vbox.append(toolbar)
+
+    win.show()
+
+
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print(f"usage: {sys.argv[0]} <file.csv>", file=sys.stderr)
-        exit(1)
+    app = Gtk.Application(
+        application_id='org.matplotlib.examples.EmbeddingInGTK4PanZoom')
+    app.connect('activate', on_activate)
+    app.run(None)
 
-    program = sys.argv[0]
-    filename = sys.argv[1]
+    # if len(sys.argv) != 2:
+    #     print(f"usage: {sys.argv[0]} <file.csv>", file=sys.stderr)
+    #     exit(1)
 
-    df = None
-    try:
-        df = pd.read_csv(filename)
-    except Exception:
-        error(f"Error reading {filename}")
-        exit(1)
-    if df is None:
-        error(f"Error reading {filename}")
-        exit(1)
+    # program = sys.argv[0]
+    # filename = sys.argv[1]
 
-    x = df.iloc[:, 0]
+    # df = None
+    # try:
+    #     df = pd.read_csv(filename)
+    # except Exception:
+    #     error(f"Error reading {filename}")
+    #     exit(1)
+    # if df is None:
+    #     error(f"Error reading {filename}")
+    #     exit(1)
 
-    fig, ax = plt.subplots()
-    for column in df.columns:
-        if column != x.name:
-            y = df[column]
-            ax.plot(x, y, label=column)
+    # x = df.iloc[:, 0]
 
-    ax.set_title(f"{filename}")
-    ax.legend()
+    # fig, ax = plt.subplots()
+    # for column in df.columns:
+    #     if column != x.name:
+    #         y = df[column]
+    #         ax.plot(x, y, label=column)
 
-    # Embed the plot in a GTK window
-    win = Gtk.Window()
-    win.connect("destroy", Gtk.main_quit)
-    win.set_default_size(600, 400)
+    # ax.set_title(f"{filename}")
+    # ax.legend()
 
-    canvas = FigureCanvasGTK3(fig)
-    win.add(canvas)
+    # # Embed the plot in a GTK window
+    # win = Gtk.Window()
+    # win.connect("destroy", Gtk.main_quit)
+    # win.set_default_size(600, 400)
 
-    win.show_all()
-    Gtk.main()
+    # canvas = FigureCanvasGTK3(fig)
+    # win.add(canvas)
 
-# this works, but how to enable mouse interaction?
+    # win.show_all()
+    # Gtk.main()
+
+# # this works, but how to enable mouse interaction?
